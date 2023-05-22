58016921df5dcde502709a014cf95121f7ac0631
reboot16 <abdiaruan99@gmail.com>
2023-05-22 15:51:47 +0700

    First Commit

---
+++ src/test/java/parkeetest/perpustakaan/PerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package parkeetest.perpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
